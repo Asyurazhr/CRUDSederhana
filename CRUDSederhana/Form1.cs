0aafdd4ad7827db80fcd542ec498c3ca1418540e
Asyurazhr <Asyuradjola2004@gmail.com>
2025-03-21 00:25:36 +0700

    Query untuk menambahkan data mahasiswa

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -73,4 +73,5 @@ namespace CRUDSederhana
                     }
 
                     conn.Open();
-                   
+                    string query = "INSERT INTO Mahasiswa (NIM, Nama, Email, Telepon, Alamat) VALUES (@NIM, @Nama, @Email, @Telepon, @Alamat)";
+                  
