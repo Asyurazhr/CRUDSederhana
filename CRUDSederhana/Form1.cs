16f40d0d40699479a3f4f921ce54fc5898f44e36
Asyurazhr <Asyuradjola2004@gmail.com>
2025-03-21 00:18:19 +0700

    Membuat DataTable untuk menyimpan hasil query

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -44,4 +44,6 @@ namespace CRUDSederhana
                 {
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
-                    SqlDataAdapter da = new SqlDataAdapter(query, conn); 
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    
