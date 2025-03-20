341444142945da5d3dbf4979d24ac2f06fc72300
Asyurazhr <Asyuradjola2004@gmail.com>
2025-03-21 00:26:08 +0700

    Menyiapkan perintah SQL dengan query

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -74,4 +74,5 @@ namespace CRUDSederhana
 
                     conn.Open();
                     string query = "INSERT INTO Mahasiswa (NIM, Nama, Email, Telepon, Alamat) VALUES (@NIM, @Nama, @Email, @Telepon, @Alamat)";
-                  
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
