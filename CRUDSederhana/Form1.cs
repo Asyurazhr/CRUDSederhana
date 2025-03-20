6aa57b0851b9d334d9e9fc421f059f084ad3dbb8
Asyurazhr <Asyuradjola2004@gmail.com>
2025-03-21 00:42:54 +0700

    Menampilkan data mahasiswa yang dipilih pada form input

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -157,4 +157,12 @@ namespace CRUDSederhana
             {
                 DataGridViewRow row = dgvMahasiswa.Rows[e.RowIndex];
 
-              
+                txtNIM.Text = row.Cells[0].Value.ToString();
+                txtNama.Text = row.Cells[1].Value.ToString();
+                txtEmail.Text = row.Cells[2].Value.ToString();
+                txtTelepon.Text = row.Cells[3].Value.ToString();
+                txtAlamat.Text = row.Cells[5].Value.ToString();
+            }
+        }
+    }
+}
